f2e26acd1ef02b836c93d4b7662c022715f9eb2d
Adeola23 <amosadeola92@gmail.com>
2023-04-01 16:46:58 +0100

    feature to detect runtime errors

--- src/main/java/org/example/Interpreter.java
+++ src/main/java/org/example/Interpreter.java
@@ -10,12 +10,26 @@ class Interpreter implements Expr.Visitor<Object> {
     public Object visitUnaryExpr(Expr.Unary expr){
         Object right = evaluate(expr.right);
 
-        return switch (expr.operator.type) {
-            case BANG -> !isTruthy(right);
-            case MINUS -> -(double) right;
-            default -> null;
-        };
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                checkNumberOperands(expr.operator, right);
+                return -(double) right;
+            default:
+                return null;
+        }
 
+    }
+
+    private void checkNumberOperands(Token operator, Object operand){
+        if(operand instanceof Double) return ;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right){
+        if(left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be numbers");
     }
 
     private boolean isTruthy(Object object){
@@ -54,23 +68,34 @@ class Interpreter implements Expr.Visitor<Object> {
                     return (double)left + (double)right;
                 }
 
-                if(left instanceof String && right instanceof String){
-                    return (String)left + (String) right;
+                if(left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
                 }
-                break;
+                throw new RuntimeError(expr.operator, "Operands must be two numbers");
+
+
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left > (double) right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left >= (double) right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left < (double) right;
+            case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left <= (double) right;
             case BANG_EQUAL: return  !isEqual(left, right);
             case EQUAL_EQUAL: return isEqual(left, right);
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left - (double)right;
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left / (double) right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return(double)left * (double)right;
 
         }
