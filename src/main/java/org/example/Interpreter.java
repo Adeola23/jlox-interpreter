7361f8b467dd366ed5710894dcede3041f362a0a
Adeola23 <amosadeola92@gmail.com>
2023-04-01 20:23:43 +0100

    public api and double precision numbers

--- src/main/java/org/example/Interpreter.java
+++ src/main/java/org/example/Interpreter.java
@@ -1,6 +1,18 @@
 package org.example;
 
 class Interpreter implements Expr.Visitor<Object> {
+    void interpret(Expr expression){
+        try{
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+
+        }catch (RuntimeError error){
+            Lox.runtimeError(error);
+        }
+    }
+
+
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr){
         return expr.value;
@@ -54,6 +66,23 @@ class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr){
         return expr.accept(this);
+    }
+
+
+    //Lox uses double-precision numbers even for integer values. In that case,
+    //they should print without a decimal point.
+
+    private String stringify(Object object){
+        if(object == null) return "nil";
+        if(object instanceof Double){
+            String text = object.toString();
+            if(text.endsWith(".0")){
+                text = text.substring(0, text.length()-2);
+            }
+            return text;
+        }
+
+        return object.toString();
     }
 
     @Override
