916db72408efdd4a4712771ebba43865d798e1b8
Adeola23 <amosadeola92@gmail.com>
2023-04-07 23:33:31 +0100

    Logical operators

--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -111,7 +111,7 @@ class Parser {
     }
 
     private Expr assignment(){
-        Expr expr = equality();
+        Expr expr = or();
         if(match(EQUAL)){
             Token equals = previous();
             Expr value = assignment();
@@ -126,6 +126,28 @@ class Parser {
         return expr;
 
     }
+
+    private Expr or(){
+        Expr expr = and ();
+        while (match(OR)){
+            Token operator = previous();
+            Expr right = and();
+            expr = new Expr.Logical(expr, operator, right);
+
+        }
+        return expr;
+    }
+
+    private Expr and(){
+        Expr expr = equality();
+        while (match(AND)){
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+        return expr;
+    }
+
 
     // The first comparison nonterminal in the body translates
     // to the first call to the comparison() in the method.
